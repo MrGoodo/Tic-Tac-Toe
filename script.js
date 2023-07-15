d32fe7bc0a6cf626ab9ebd6fe18eccd30af710cb
Mrgoodo <goodo.nick@gmail.com>
2023-07-15 10:38:06 +0400

    Завершение проекта, добавление кнопки reset

---
+++ script.js
@@ -0,0 +1,114 @@
+let currentPlayer = "X";
+let gameBoard = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+
+const checkWin = () => {
+  for (let row = 0; row < 3; row++) {
+    if (
+      gameBoard[row][0] === currentPlayer &&
+      gameBoard[row][1] === currentPlayer &&
+      gameBoard[row][2] === currentPlayer
+    ) {
+      return true;
+    }
+  }
+  for (let col = 0; col < 3; col++) {
+    if (
+      gameBoard[0][col] === currentPlayer &&
+      gameBoard[1][col] === currentPlayer &&
+      gameBoard[2][col] === currentPlayer
+    ) {
+      return true;
+    }
+  }
+  if (
+    gameBoard[0][0] === currentPlayer &&
+    gameBoard[1][1] === currentPlayer &&
+    gameBoard[2][2] === currentPlayer
+  ) {
+    return true;
+  }
+  if (
+    gameBoard[0][2] === currentPlayer &&
+    gameBoard[1][1] === currentPlayer &&
+    gameBoard[2][0] === currentPlayer
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+const checkDraw = () => {
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      if (gameBoard[row][col] === "") {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const makeMove = (row, col) => {
+  const info = document.querySelector(".info");
+  if (gameBoard[row][col] === "") {
+    gameBoard[row][col] = currentPlayer;
+
+    if (checkWin()) {
+      info.innerText = `Player ${currentPlayer} win!`;
+      resetGame();
+    } else if (checkDraw()) {
+      info.innerText = "Draw!";
+      resetGame();
+    } else {
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
+  }
+};
+
+const resetGame = () => {
+  currentPlayer = "X";
+  gameBoard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  renderBoard();
+};
+
+const renderBoard = () => {
+  const board = document.querySelector(".board");
+  board.innerHTML = "";
+
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      cell.innerText = gameBoard[row][col];
+      cell.addEventListener("click", () => {
+        makeMove(row, col);
+        renderBoard();
+      });
+
+      board.appendChild(cell);
+    }
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => renderBoard());
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderBoard();
+
+  const resetButton = document.querySelector(".resetButton");
+  resetButton.addEventListener("click", () => {
+    resetGame();
+    const info = document.querySelector(".info");
+    info.innerText = ""; // Очистка текста информации
+    renderBoard();
+  });
+});
